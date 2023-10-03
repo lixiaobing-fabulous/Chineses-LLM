e44899294861a527d32172d847f3aa28fb0cf792
lixiaobing <lixiaobing_master@126.com>
2023-10-03 16:53:47 +0800

    init a gpt prototype 初始化gpt原型代码

---
+++ model.py
@@ -0,0 +1,152 @@
+import math
+from dataclasses import dataclass
+
+import torch
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+@dataclass
+class GPTConfig:
+    block_size: int = 32
+    vocab_size: int = 65
+    n_layer: int = 4
+    n_head: int = 4
+    n_embed: int = 64
+    dropout: float = 0.0
+    bias: bool = False
+
+
+class LayerNorm(nn.Module):
+
+    def __init__(self, ndim):
+        super().__init__()
+        self.layer_norm = nn.LayerNorm(ndim)
+
+    def forward(self, x):
+        return self.layer_norm(x)
+
+
+class SelfAttention(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        assert config.n_embed % config.n_head == 0
+        self.attn = nn.Linear(config.n_embed, 3 * config.n_embed, bias=config.bias)
+        self.proj = nn.Linear(config.n_embed, config.n_embed, bias=config.bias)
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.resid_dropout = nn.Dropout(config.dropout)
+        self.n_head = config.n_head
+        self.n_embed = config.n_embed
+        self.dropout = config.dropout
+        self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                             .view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x):
+        B, T, C = x.size()  # batch_size, sequence_length, embedding dimensionality
+        attn_layer = self.attn(x)  # calculate query, key, values for all heads
+        q, k, v = attn_layer.split(self.n_embed, dim=2)  # split query, key, values
+        q = q.view(B, T, self.n_head, self.n_embed // self.n_head)  # (B, T, n_head, head_size)
+        k = k.view(B, T, self.n_head, self.n_embed // self.n_head)  # (B, T, n_head, head_size)
+        v = v.view(B, T, self.n_head, self.n_embed // self.n_head)  # (B, T, n_head, head_size)
+
+        q = q.transpose(1, 2)  # (B, n_head, T, head_size)
+        k = k.transpose(1, 2)  # (B, n_head, T, head_size)
+        v = v.transpose(1, 2)  # (B, n_head, T, head_size)
+
+        attn = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(k.size(-1)))
+        attn = attn.masked_fill(self.bias[:, :, :T, :T] == 0, float('-inf'))
+        attn = F.softmax(attn, dim=-1)
+        attn = self.attn_dropout(attn)
+        y = attn @ v  # （B, nh, T, T) x (B, nh, T, hs)->(B, nh, T, hs)
+        y = y.transpose(1, 2).contiguous().view(B, T, C)
+        y = self.resid_dropout(y)
+        return y
+
+
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.fc = nn.Linear(config.n_embed, 4 * config.n_embed, bias=config.bias)
+        self.gelu = nn.GELU()
+        self.project = nn.Linear(4 * config.n_embed, config.n_embed, bias=config.bias)
+        self.dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x):
+        x = self.fc(x)
+        x = self.gelu(x)
+        x = self.project(x)
+        x = self.dropout(x)
+        return x
+
+
+class Block(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        self.ln_1 = LayerNorm(config.n_embed)
+        self.attn = SelfAttention(config)
+        self.ln_2 = LayerNorm(config.n_embed)
+        self.mlp = MLP(config)
+
+    def forward(self, x):
+        x = x + self.attn(self.ln_1(x))
+        x = x + self.mlp(self.ln_2(x))
+        return x
+
+
+class GPT(nn.Module):
+
+    def __init__(self, config):
+        super().__init__()
+        # do some validation
+        assert config.vocab_size is not None
+        assert config.block_size is not None
+        assert config.n_embed is not None
+        assert config.n_layer is not None
+        assert config.dropout is not None
+        self.config = config
+
+        self.transformer = nn.ModuleDict(dict(
+            word_embedding=nn.Embedding(config.vocab_size, config.n_embed),
+            positional_embedding=nn.Embedding(config.block_size, config.n_embed),
+            dropout=nn.Dropout(config.dropout),
+            blocks=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
+            layer_norm=LayerNorm(config.n_embed)
+        ))
+        self.linear_transform = nn.Linear(config.n_embed, config.vocab_size, bias=False)
+
+        # print("number of parameters: %.2fM" % (self.get_num_params() / 1e6))
+
+    # def get_num_params(self):
+    #     return len(self.parameters())
+
+    def forward(self, idx, targets=None):
+        device = idx.device
+        b, t = idx.size()
+        assert t <= self.config.block_size
+        pos = torch.arange(0, t, dtype=torch.long, device=device)
+        word_emb = self.transformer.word_embedding(idx)
+        pos_emb = self.transformer.positional_embedding(pos)
+        x = self.transformer.dropout(word_emb + pos_emb)
+        for block in self.transformer.blocks:
+            x = block(x)
+        x = self.transformer.layer_norm(x)
+        if targets is not None:
+            logits = self.linear_transform(x)
+            loss = F.cross_entropy(logits.view(-1, logits.size(-1)), targets.view(-1))
+        else:
+            logits = self.linear_transform(x[:, [-1], :])
+            loss = None
+        return logits, loss
+
+    def generate(self, idx, max_new_tokens, temperature=1.0):
+        for _ in range(max_new_tokens):
+            idx_truncated = idx if idx.size(1) <= self.config.block_size else idx[:, -self.config.block_size:]
+            logits, _ = self(idx_truncated)
+            logits = logits[:, -1, :] / temperature
+            probs = F.softmax(logits, dim=-1)
+            idx_next = torch.multinomial(probs, num_samples=1)
+            idx = torch.cat((idx, idx_next), dim=1)
+        return idx
+
---
+++ train.py
@@ -0,0 +1,69 @@
+from model import GPTConfig, GPT
+import torch
+
+learning_rate = 1e-3
+max_iters = 5000
+eval_interval = 100
+eval_iters = 200
+block_size = 32
+batch_size = 16
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+gpt_conf = GPTConfig()
+model = GPT(gpt_conf)
+model.to(device)
+with open('input.txt', 'r', encoding='utf-8') as f:
+    text = f.read()
+chars = sorted(list(set(text)))
+vocab_size = len(chars)
+stoi = {ch: i for i, ch in enumerate(chars)}
+itos = {i: ch for i, ch in enumerate(chars)}
+
+encode = lambda s: [stoi[c] for c in s]
+decode = lambda l: ''.join([itos[i] for i in l])
+data = torch.tensor(encode(text), dtype=torch.long)
+n = int(0.9 * len(data))
+train_data = data[:n]
+val_data = data[n:]
+
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
+
+
+
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i + block_size] for i in ix])
+    y = torch.stack([data[i + 1:i + block_size + 1] for i in ix])
+    x, y = x.to(device), y.to(device)
+    return x, y
+
+
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
+for iter in range(max_iters):
+    if iter % eval_interval == 0 or iter == max_iters - 1:
+        losses = estimate_loss()
+        print(f"stemp {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
+    xb, yb = get_batch('train')
+
+    logits, loss = model(xb, yb)
+    optimizer.zero_grad(set_to_none=True)
+    loss.backward()
+    optimizer.step()
+context = torch.zeros((1,1), dtype=torch.long, device=device)
+print(decode(model.generate(context, max_new_tokens=2000)[0].tolist()))
